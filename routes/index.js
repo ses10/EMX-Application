eba3d5598148905910a0d79082e63af4a4b950ad
ses10 <sesma@live.com>
2018-11-03 15:30:46 -0400

    log request info

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 
-  res.send('req');
+  console.log(req);
+  res.send('test');
 
 });
 
