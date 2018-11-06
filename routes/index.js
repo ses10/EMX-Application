18195d32218f501fee5a5a35140f23a71017ff8c
ses10 <sesma@live.com>
2018-11-05 21:44:48 -0500

    add solvePuzzle and getOppositeOperator

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.get('/', function(req, res, next) {
   if(resume[req.query.q] !== undefined)
   	res.send(resume[req.query.q]); 
   else if (req.query.q === 'Puzzle') {
-  	console.log(extractPuzzle(req.query.d));
-
+  	//console.log(extractPuzzle(req.query.d));
+  	res.send(solvePuzzle(extractPuzzle(req.query.d)));
   }
   res.send('OK');	
   
@@ -45,10 +45,112 @@ function extractPuzzle(str){
   	return puzzle;
 }
 
-function solvePuzzle(){
+/*
+	TODO
+		high level explanation
+*/
+function solvePuzzle(puzzle){
+	//console.log(puzzle);
+	var givenRowValues = {};
+
+	//find the 3 given operators
+	for(var row = 0; row < puzzle.length; row++)
+	{
+		for(var col = 0; col < puzzle[row].length; col++)
+		{
+			if(puzzle[row][col] !== '-')
+			{
+				var rowObj = {};
+				rowObj['value'] = puzzle[row][col];
+				rowObj['location'] = {'row' : row, 'col' : col};
+				givenRowValues[row] = rowObj;
+				break;
+			}
+		}		
+	}
+	//console.log(givenRowValues);
+	
+
+	//assign the opposite values for given 3
+	for(var i = 0; i < Object.keys(givenRowValues).length; i++)
+	{
+		var row = givenRowValues[i].location.row;
+		var col = givenRowValues[i].location.col;
+
+		var operator = getOppositeComparisonOperator(puzzle[row][col]);
+
+		if(operator !== undefined)
+			puzzle[col][row] = operator;
+	}
+	//console.log(puzzle);
 
 
+	//fill in the = operator
+	puzzle[0][0] = '=';
+	puzzle[1][1] = '=';
+	puzzle[2][2] = '=';
+	puzzle[3][3] = '=';
+	//console.log(puzzle);
 
+	//fill in 5 of the remaining 6 left
+	for(var row = 0; row < puzzle.length; row++)
+	{
+		for(var col = 0; col < puzzle[row].length; col++)
+		{
+			if(puzzle[row][col] === '-')
+			{
+				var operator = givenRowValues[row]['value'];
+
+				if(operator !== '=')
+					puzzle[row][col] = operator;
+			}
+		}
+	}	
+	//console.log(puzzle);
+
+	//fill in the last value in puzzle
+	var lastRow = "";
+	var lastCol = "";
+	for(var row = 0; row < puzzle.length; row++)
+	{
+		var lastFound = false;
+
+		for(var col = 0; col < puzzle[row].length; col++)
+		{
+			if(puzzle[row][col] == '-')
+			{
+				lastRow = row;
+				lastCol = col;
+				lastFound = true;
+				break;
+			}
+		}
+		if(lastFound)
+			break;
+	}	
+
+	puzzle[lastRow][lastCol] = getOppositeComparisonOperator(puzzle[lastCol][lastRow]);
+
+	puzzleStr = " ABCD\n";
+	puzzleStr += "A" + puzzle[0].join("") + "\n";
+	puzzleStr += "B" + puzzle[1].join("") + "\n";
+	puzzleStr += "C" + puzzle[2].join("") + "\n";
+	puzzleStr += "D" + puzzle[3].join("") + "\n";
+	
+	//console.log(puzzleStr);
+
+
+	return puzzleStr;
+
+}
+
+function getOppositeComparisonOperator(operator){
+	if(operator === '>')
+		return '<';
+	else if(operator === '<')
+		return '>';
+	else
+		return undefined;
 }
 
 module.exports = router;
